a32895bb1d11775fc49221137cc675e185c3937c
Alexander-Sobolev <sobolev.ios.developer@gmail.com>
2021-03-25 10:47:40 +0300

    Created new Model

---
+++ RestingPlaces/PlaceModel.swift
@@ -0,0 +1,36 @@
+//
+//  PlaceModel.swift
+//  RestingPlaces
+//
+//  Created by Alexander Sobolev on 25.03.2021.
+//
+
+import Foundation
+
+struct Place {
+    
+    var name: String
+    var location: String
+    var type: String
+    var image: String
+    
+    static let restaurantNames = [
+        "Burger Heroes", "Kitchen", "Bonsai", "Дастархан",
+        "Индокитай", "X.O", "Балкан Гриль", "Sherlock Holmes",
+        "Speak Easy", "Morris Pub", "Вкусные истории",
+        "Классик", "Love&Life", "Шок", "Бочка"
+    ]
+    
+    static func getPlaces() -> [Place] {
+        
+        var places = [Place]()
+        
+        for place in restaurantNames {
+            places.append(Place(name: place, location: "Vancouver", type: "Movies", image: place))
+        }
+        
+        return places
+        
+    }
+    
+}
