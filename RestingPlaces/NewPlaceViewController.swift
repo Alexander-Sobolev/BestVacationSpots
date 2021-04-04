0e5ba296969d810cfa8fe370627bb6c893056717
Alexander-Sobolev <sobolev.ios.developer@gmail.com>
2021-04-04 15:34:10 +0300

    Created AlertController

---
+++ RestingPlaces/NewPlaceViewController.swift
@@ -0,0 +1,70 @@
+//
+//  NewPlaceViewController.swift
+//  RestingPlaces
+//
+//  Created by Alexander Sobolev on 30.03.2021.
+//
+
+import UIKit
+
+class NewPlaceViewController: UITableViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        tableView.tableFooterView = UIView()
+        
+    }
+    
+    // MARK: - Table View Delegate
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        if indexPath.row == 0 {
+            
+            let actionSheet = UIAlertController(title: nil, message: nil, preferredStyle: .actionSheet)
+            
+            let photo = UIAlertAction(title: "Photo", style: .default) { _ in
+                self.chooseImagePicker(sourse: .photoLibrary)
+            }
+            
+            let camera = UIAlertAction(title: "Camera", style: .default) { _ in
+                self.chooseImagePicker(sourse: .camera)
+            }
+            
+            let cancel = UIAlertAction(title: "Cancel", style: .cancel)
+            
+            actionSheet.addAction(photo)
+            actionSheet.addAction(cancel)
+            actionSheet.addAction(camera)
+            
+            present(actionSheet, animated: true)
+            
+        } else {
+            view.endEditing(true)
+        }
+    }
+}
+
+
+// MARK: - Text Field Delegate
+
+extension NewPlaceViewController: UITextFieldDelegate {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+}
+
+// MARK: - Work With Image
+extension NewPlaceViewController {
+    
+    func chooseImagePicker(sourse: UIImagePickerController.SourceType) {
+        if UIImagePickerController.isSourceTypeAvailable(sourse) {
+            let imagePicker = UIImagePickerController()
+            imagePicker.allowsEditing = true
+            imagePicker.sourceType = sourse
+            present(imagePicker, animated: true)
+        }
+    }
+    
+}
